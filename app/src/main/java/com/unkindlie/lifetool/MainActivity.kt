ebf4305a5eee069a852e72d88ccf2208f851c81c
unkindlie <inoutoftheverge@gmail.com>
2025-05-08 22:40:25 +0300

    chore: material theme

--- app/src/main/java/com/unkindlie/lifetool/MainActivity.kt
+++ app/src/main/java/com/unkindlie/lifetool/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.unkindlie.lifetool.ui.theme.LifeToolTheme
@@ -32,10 +34,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = name)
+    }
 }
 
 @Preview(showBackground = true)
