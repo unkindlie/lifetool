1f53aadb81ef861e24c245b9f0af55d001da4062
unkindlie <inoutoftheverge@gmail.com>
2025-05-11 00:05:29 +0300

    chore: fonts

--- app/src/main/java/com/unkindlie/lifetool/MainActivity.kt
+++ app/src/main/java/com/unkindlie/lifetool/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.unkindlie.lifetool.ui.theme.LifeToolTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +36,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = name)
+        Text(text = name, fontSize = 28.sp)
     }
 }
 
--- app/src/main/java/com/unkindlie/lifetool/ui/theme/Type.kt
+++ app/src/main/java/com/unkindlie/lifetool/ui/theme/Type.kt
@@ -1,9 +1,40 @@
 package com.unkindlie.lifetool.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.unkindlie.lifetool.R
 
-val AppTypography = Typography()
+val productSansFamily = FontFamily(
+    Font(R.font.productsans_thin, FontWeight.Thin),
+    Font(R.font.productsans_light, FontWeight.Light),
+    Font(R.font.productsans_regular, FontWeight.Normal),
+    Font(R.font.productsans_medium, FontWeight.Medium),
+    Font(R.font.productsans_bold, FontWeight.Bold),
+    Font(R.font.productsans_black, FontWeight.Bold)
+)
+
+val defaultTypography = Typography()
+
+val AppTypography = Typography(
+    displayLarge = defaultTypography.displayLarge.copy(fontFamily = productSansFamily),
+    displayMedium = defaultTypography.displayMedium.copy(fontFamily = productSansFamily),
+    displaySmall = defaultTypography.displaySmall.copy(fontFamily = productSansFamily),
+
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = productSansFamily),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = productSansFamily),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = productSansFamily),
+
+    titleLarge = defaultTypography.titleLarge.copy(fontFamily = productSansFamily),
+    titleMedium = defaultTypography.titleMedium.copy(fontFamily = productSansFamily),
+    titleSmall = defaultTypography.titleSmall.copy(fontFamily = productSansFamily),
+
+    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = productSansFamily),
+    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = productSansFamily),
+    bodySmall = defaultTypography.bodySmall.copy(fontFamily = productSansFamily),
+
+    labelLarge = defaultTypography.labelLarge.copy(fontFamily = productSansFamily),
+    labelMedium = defaultTypography.labelMedium.copy(fontFamily = productSansFamily),
+    labelSmall = defaultTypography.labelSmall.copy(fontFamily = productSansFamily)
+)
