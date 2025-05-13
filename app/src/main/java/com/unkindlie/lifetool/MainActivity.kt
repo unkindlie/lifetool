a7f4fb3d9566cfcdb17ca935544ead8b2fb79363
unkindlie <inoutoftheverge@gmail.com>
2025-05-13 12:51:24 +0300

    chore: code cleaning

--- app/src/main/java/com/unkindlie/lifetool/MainActivity.kt
+++ app/src/main/java/com/unkindlie/lifetool/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.unkindlie.lifetool.ui.theme.LifeToolTheme
 
@@ -37,13 +36,5 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Text(text = name, fontSize = 28.sp)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LifeToolTheme {
-        Greeting("Android")
     }
 }
