e5551f939310d2f00834ba7c44b4c77bfceb3913
unkindlie <inoutoftheverge@gmail.com>
2025-05-13 17:28:00 +0300

    chore: dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -39,8 +41,9 @@ android {
     }
 }
 
-dependencies {
+//noinspection UseTomlInstead GradleDependency
 
+dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,4 +59,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    implementation("io.github.raamcosta.compose-destinations:core:1.11.9")
+    ksp("io.github.raamcosta.compose-destinations:ksp:1.11.9")
 }
